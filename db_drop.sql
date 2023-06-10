0e82539dd1a49232ff86599ff27dfb20f0c76491
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-06-09 23:49:57 -0400

    Add "uninstallation script" for database so I can update the schema easier

---
+++ db_drop.sql
@@ -0,0 +1,22 @@
+﻿/*
+ * db_drop.sql
+ * Schema Version: 1.0.0
+ *
+ * WARNING: Dropping tables removes all data in that table. Save your data if needed
+ * Run this file by uncommenting the "COMMIT TRAN" and commenting "ROLLBACK TRAN"
+ * This file is intended to "uninstall" your database to allow for schema change udpates.
+ */
+
+BEGIN TRAN
+
+DROP TABLE Album
+DROP TABLE LinkedTracks
+DROP TABLE ListArtist
+DROP TABLE ListExtension
+DROP TABLE ListGenre
+DROP TABLE ListMood
+DROP TABLE ListOwner
+DROP TABLE Main
+
+ROLLBACK TRAN
+--COMMIT TRAN
