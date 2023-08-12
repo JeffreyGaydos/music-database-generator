7e5d26f266803a8d4215a32379f8afc6ed44099d
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-08-12 18:28:08 -0400

    update example sql scripts

--- Schema/db_drop.sql
+++ Schema/db_drop.sql
@@ -11,20 +11,70 @@ BEGIN TRAN
 
 USE MusicLibrary
 
-DROP TABLE Mood
-DROP TABLE MoodTracks
-DROP TABLE Playlist
-DROP TABLE PlaylistTracks
-DROP TABLE Artist
-DROP TABLE ArtistTracks
-DROP TABLE ArtistPersons
-DROP TABLE Genre
-DROP TABLE GenreTracks
-DROP TABLE Album
-DROP TABLE AlbumTracks
-DROP TABLE LinkedTracks
-DROP TABLE PlayLogs
-DROP TABLE Main
+IF (SELECT [dbo].[MusicTableExists] (N'Mood')) = 1
+BEGIN
+    DROP TABLE Mood
+END
+IF (SELECT [dbo].[MusicTableExists] (N'MoodTracks')) = 1
+BEGIN
+    DROP TABLE MoodTracks
+END
+IF (SELECT [dbo].[MusicTableExists] (N'Playlist')) = 1
+BEGIN
+    DROP TABLE Playlist
+END
+IF (SELECT [dbo].[MusicTableExists] (N'PlaylistTracks')) = 1
+BEGIN
+    DROP TABLE PlaylistTracks
+END
+IF (SELECT [dbo].[MusicTableExists] (N'Artist')) = 1
+BEGIN
+    DROP TABLE Artist
+END
+IF (SELECT [dbo].[MusicTableExists] (N'ArtistTracks')) = 1
+BEGIN
+    DROP TABLE ArtistTracks
+END
+IF (SELECT [dbo].[MusicTableExists] (N'ArtistPersons')) = 1
+BEGIN
+    DROP TABLE ArtistPersons
+END
+IF (SELECT [dbo].[MusicTableExists] (N'Genre')) = 1
+BEGIN
+    DROP TABLE Genre
+END
+IF (SELECT [dbo].[MusicTableExists] (N'GenreTracks')) = 1
+BEGIN
+    DROP TABLE GenreTracks
+END
+IF (SELECT [dbo].[MusicTableExists] (N'Album')) = 1
+BEGIN
+    DROP TABLE Album
+END
+IF (SELECT [dbo].[MusicTableExists] (N'AlbumTracks')) = 1
+BEGIN
+    DROP TABLE AlbumTracks
+END
+IF (SELECT [dbo].[MusicTableExists] (N'AlbumArt')) = 1
+BEGIN
+    DROP TABLE AlbumArt
+END
+IF (SELECT [dbo].[MusicTableExists] (N'LinkedTracks')) = 1
+BEGIN
+    DROP TABLE LinkedTracks
+END
+IF (SELECT [dbo].[MusicTableExists] (N'PlayLogs')) = 1
+BEGIN
+    DROP TABLE PlayLogs
+END
+IF (SELECT [dbo].[MusicTableExists] (N'Main')) = 1
+BEGIN
+    DROP TABLE Main
+END
+IF (SELECT [dbo].[MusicTableExists] (N'__MigrationHistory')) = 1
+BEGIN
+    DROP TABLE __MigrationHistory
+END
 
 DROP VIEW [MainDataJoined]
 DROP VIEW [LeadArtists]
--- Schema/db_example.sql
+++ Schema/db_example.sql
@@ -1,11 +1,14 @@
 SELECT M.*, ATR.TrackOrder, A.AlbumName, A.ReleaseYear, G.GenreName, ART.ArtistName FROM MusicLibrary.dbo.Main M
 JOIN MusicLibrary.dbo.AlbumTracks ATR ON M.TrackID = ATR.TrackID
 JOIN MusicLibrary.dbo.Album A ON A.AlbumID = ATR.AlbumID
-JOIN MusicLibrary.dbo.GenreTracks GTR ON GTR.TrackID = M.TrackID
-JOIN MusicLibrary.dbo.Genre G ON G.GenreID = GTR.GenreID
+LEFT JOIN MusicLibrary.dbo.GenreTracks GTR ON GTR.TrackID = M.TrackID
+LEFT JOIN MusicLibrary.dbo.Genre G ON G.GenreID = GTR.GenreID
 JOIN MusicLibrary.dbo.ArtistTracks ARTT ON ARTT.TrackID = M.TrackID
 JOIN MusicLibrary.dbo.Artist ART ON ART.ArtistID = ARTT.ArtistID
 
 SELECT ART.*, AP.*, PRP.PersonName AS PrimaryPerson FROM MusicLibrary.dbo.Artist ART
 LEFT JOIN MusicLibrary.dbo.ArtistPersons AP ON ART.ArtistID = AP.ArtistID
 LEFT JOIN MusicLibrary.dbo.ArtistPersons PRP ON PRP.PersonID = ART.PrimaryPersonID
+
+SELECT * FROM MusicLibrary.dbo.AlbumArt ART
+JOIN MusicLibrary.dbo.Album ALB ON ALB.AlbumID = ART.AlbumID
