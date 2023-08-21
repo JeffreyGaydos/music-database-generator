a001e852ab76785eb5f1f6b431f2da552e9ce089
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-08-20 21:13:04 -0400

    Add loggers; use alternative metadata when primary are missing

--- Generators/ArtistPersonGenerator.cs
+++ Generators/ArtistPersonGenerator.cs
@@ -8,20 +8,47 @@ namespace MusicDatabaseGenerator.Generators
 {
     public class ArtistPersonGenerator : AGenerator, IGenerator
     {
-        public ArtistPersonGenerator(TagLib.File file, MusicLibraryTrack data)
+        public ArtistPersonGenerator(TagLib.File file, MusicLibraryTrack data, LoggingUtils logger)
         {
             _file = file;
             _data = data;
+            _logger = logger;
         }
 
         public void Generate()
         {
             foreach(string person in _file.Tag.Composers)
             {
-                _data.artistPersons.Add(new ArtistPersons
+                if(string.IsNullOrWhiteSpace(person))
                 {
-                    PersonName = person,
-                });
+                    _data.artistPersons.Add(new ArtistPersons
+                    {
+                        PersonName = person,
+                    });
+                }
+            }
+            if(_data.artistPersons.Count == 0)
+            {
+                foreach (string person in _file.Tag.Performers)
+                {
+                    if (string.IsNullOrWhiteSpace(person))
+                    {
+                        _data.artistPersons.Add(new ArtistPersons
+                        {
+                            PersonName = person,
+                        });
+                    }
+                }
+            }
+            if(_data.artistPersons.Count == 0)
+            {
+                //we don't really care if we can't find a primary person. Not all artists reveal this information
+                _logger.GenerationLogWriteData($"Artist Person: No personal data found for track  \"{_data.main.Title}\"...", true);
+                return;
+            } else
+            {
+                _logger.GenerationLogWriteData($"Artist Person: \"Composers\" metadata did not exist on track \"{_data.main.Title}\". Using \"Performers\" data instead...", true);
+                return;
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿using MusicDatabaseGenerator.Generators;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace MusicDatabaseGenerator
 {
@@ -11,25 +15,31 @@ namespace MusicDatabaseGenerator
     {
         static void Main(string[] args)
         {
+            LoggingUtils logger = new LoggingUtils();
             //TODO: Replace this later with a config or something
-            string musicFolder = "data2"; //point this to where the mp3s are located, relative to the top level folder of this repo
+            //string musicFolder = "../../data3"; //point this to where the mp3s are located, relative to the top level folder of this repo
             //string musicFolder = "../../../../Music";
+            string musicFolder = @"C:\Users\jeff1\Music";
 
-            (List<TagLib.File> tagFiles, List<(string, Bitmap)> coverArt) constructedTuple = FolderReader.ReadToTagLibFiles(musicFolder);
+            FolderReader.InjectDependencies(logger);
+            (List<TagLib.File> tagFiles, List<(string, Bitmap)> coverArt) constructedTuple = FolderReader.ReadToTagLibFiles(musicFolder, true);
 
             MusicLibraryContext mdbContext = new MusicLibraryContext();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 
+            int total = constructedTuple.tagFiles.Count;
             foreach (TagLib.File data in constructedTuple.tagFiles)
             {
-                MusicLibraryTrack trackData = new MusicLibraryTrack(mdbContext);
+                MusicLibraryTrack trackData = new MusicLibraryTrack(mdbContext, logger, total);
 
                 List<IGenerator> generators = new List<IGenerator>
                 {
-                    new MainGenerator(data, trackData),
+                    new MainGenerator(data, trackData, logger),
                     new GenreGenerator(data, trackData),
-                    new ArtistGenerator(data, trackData),
-                    new AlbumGenerator(data, trackData),
-                    new ArtistPersonGenerator(data, trackData)
+                    new ArtistGenerator(data, trackData, musicFolder, logger),
+                    new AlbumGenerator(data, trackData, musicFolder, logger),
+                    new ArtistPersonGenerator(data, trackData, logger)
                 };
 
                 foreach (IGenerator generator in generators)
@@ -42,10 +52,13 @@ namespace MusicDatabaseGenerator
                 trackData.Sync();
             }
 
+            total = constructedTuple.coverArt.Count;
+            logger.GenerationLogWriteComment($"Song Data Inserted Into Database in {sw.Elapsed.Seconds} seconds");
+            sw.Restart();
             //Strict ordering, album art must come second so we can match it to an album via our sync function
             foreach ((string, Bitmap) img in constructedTuple.coverArt)
             {
-                MusicLibraryTrack trackData = new MusicLibraryTrack(mdbContext);
+                MusicLibraryTrack trackData = new MusicLibraryTrack(mdbContext, logger, total);
 
                 List<IGenerator> generators = new List<IGenerator>
                 {
@@ -58,10 +71,21 @@ namespace MusicDatabaseGenerator
                 }
 
                 MusicLibraryTrack.albumArtIndex += 1;
-
-                trackData.Sync();
-                //TODO: Use regex to compare paths to determine which album the art is associated with
+                
+                try
+                {
+                    trackData.Sync();
+                }
+                catch (UpdateException ue)
+                {
+                    if (ue.Message.Contains("Violation of PRIMARY KEY constraint"))
+                    {
+                        logger.GenerationLogWriteData($"Found duplicate content for file, skipping {trackData.main.FilePath}");
+                        logger.DuplicateLogWriteData(trackData.main.FilePath);
+                    }
+                }
             }
+            logger.GenerationLogWriteComment($"Album Art Data Inserted Into Database in {sw.Elapsed.Seconds} seconds");
         }
     }
 }
