16a8554e9090ada7a729d5b67c7f3fa3ce9b9c58
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-09-04 12:14:53 -0400

    make MusicLibraryTrack strictly a data object

---
+++ SQLStringComparison.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace MusicDatabaseGenerator
+{
+    public class SQLStringComparison : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            if (x == y) return true;
+            if (x?.ToLower() == y?.ToLower()) return true;
+            return string.Compare(x?.ToLower(), y?.ToLower(), CultureInfo.InvariantCulture, CompareOptions.IgnoreNonSpace | CompareOptions.IgnoreCase | CompareOptions.IgnoreSymbols) == 0;
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return obj?.ToLower().GetHashCode() ?? 0;
+        }
+    }
+}
