e5488b7c098185b2ca1b26d5dd0427d4a78d02fb
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-09-04 11:21:27 -0400

    implement album art synchronizer with default insert

--- Synchronizers/AlbumArtSynchronizer.cs
+++ Synchronizers/AlbumArtSynchronizer.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MusicDatabaseGenerator.Synchronizers
 {
     public class AlbumArtSynchronizer : ASynchronizer, ISynchronizer
     {
+        private Regex parentDirectoryMatch = new Regex(".*(?=[\\\\\\/][^\\\\\\/]*\\.[a-zA-Z]+)", RegexOptions.Compiled);
+
         public AlbumArtSynchronizer(MusicLibraryTrack mlt, MusicLibraryContext context)
         {
             _context = context;
@@ -16,12 +19,23 @@ namespace MusicDatabaseGenerator.Synchronizers
 
         public void Synchronize()
         {
-            
+            Insert();   
         }
 
         internal override void Insert()
         {
-            base.Insert();
+            List<AlbumTracks> existingAlbumTracks = _context.AlbumTracks.ToList();
+            foreach (AlbumArt art in _mlt.albumArt)
+            {
+                string parentDirectory = parentDirectoryMatch.Match(art.AlbumArtPath).Value;
+                int trackID = _context.Main
+                    .Where(m => m.FilePath.StartsWith(parentDirectory))
+                    .Select(t => t.TrackID).FirstOrDefault();
+
+                art.AlbumID = existingAlbumTracks.Where(at => at.TrackID == trackID).FirstOrDefault()?.AlbumID;
+                _context.AlbumArt.Add(art);
+            }
+            _context.SaveChanges();
         }
 
         internal override void Update()
