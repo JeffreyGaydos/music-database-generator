fe9435c88a46ad963b5ff33a35745a7bfaa7ec5a
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-12-31 22:40:30 -0500

    Groove: parse playlist title

--- PlaylistTransferTool/GroovePlaylistParser.cs
+++ PlaylistTransferTool/GroovePlaylistParser.cs
@@ -1,5 +1,7 @@
 ﻿using MusicDatabaseGenerator;
 using System;
+using System.IO;
+using System.Xml;
 
 namespace PlaylistTransferTool
 {
@@ -7,7 +9,24 @@ namespace PlaylistTransferTool
     {
         public Playlist ParsePlaylist(string file)
         {
-            throw new NotImplementedException();
+            Playlist result = new Playlist()
+            {
+                CreationDate = DateTime.Now,
+                LastEditDate = DateTime.Now,
+                PlaylistDescription = $"Imported using the file {file} via the PlaylistTransferTool",
+                PlaylistName = "Unknown Title",
+            };
+
+            var zplFile = new StreamReader(file);
+            var zpl = zplFile.ReadToEnd();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(zpl);
+
+            var xmlHead = xmlDoc.LastChild.FirstChild;
+            result.PlaylistName = xmlHead.ChildNodes[3].InnerText;
+
+            return result;
         }
 
         public PlaylistTracks[] ParsePlaylistTracks(string file)
