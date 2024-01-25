b4ce1923f8ca1e92ff666ee9eb4b4fdd829851ec
JeffreyGaydos <55904615+JeffreyGaydos@users.noreply.github.com>
2024-01-24 21:05:21 -0500

    Use Milliseconds for Duration Instead of Seconds; Re-Add Generated Date (#32)
    * huh, I guess we never actually added the generation date did we
    
    * Switch duration to miliseconds
    
    * update readme to describe millisecond units on duration field

--- MusicDatabaseGenerator/Generators/MainGenerator.cs
+++ MusicDatabaseGenerator/Generators/MainGenerator.cs
@@ -29,9 +29,10 @@ namespace MusicDatabaseGenerator.Generators
             {
                 Title = PVU.PrevalidateStringTruncate(title, 435, nameof(Main.Title)),
                 FilePath = PVU.PrevalidateStringTruncate(Path.GetFullPath(_file.Name), 260, nameof(Main.FilePath)),
-                Duration = PVU.PrevalidateDoubleToDecimalCast(Math.Round(_file.Properties.Duration.TotalSeconds), nameof(Main.Duration)),
+                Duration = PVU.PrevalidateDoubleToDecimalCast(Math.Round(_file.Properties.Duration.TotalMilliseconds), nameof(Main.Duration)),
                 ReleaseYear = (int?)PVU.PrevalidateUnsignedIntToIntCast(_file.Tag.Year, nameof(Main.ReleaseYear)) == 0 ? null : (int?)PVU.PrevalidateUnsignedIntToIntCast(_file.Tag.Year, nameof(Main.ReleaseYear)),
                 AddDate = new FileInfo(_file.Name).CreationTime,
+                GeneratedDate = DateTime.Now,
                 LastModifiedDate = new FileInfo(_file.Name).LastWriteTime,
                 Lyrics = PVU.PrevalidateStringTruncate(_file.Tag.Lyrics, 4000, nameof(Main.Lyrics)),
                 Comment = PVU.PrevalidateStringTruncate(_file.Tag.Comment, 4000, nameof(Main.Comment)),
