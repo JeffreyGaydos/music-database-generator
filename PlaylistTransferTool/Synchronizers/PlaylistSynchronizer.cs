c05042cef6e72c9e97c117259f5d72576b666a80
JeffreyGaydos <jeff13gaydos@gmail.com>
2024-07-25 21:13:30 -0400

    delete the playlist and all tracks if we are not merging and find an existing playlist

--- PlaylistTransferTool/Synchronizers/PlaylistSynchronizer.cs
+++ PlaylistTransferTool/Synchronizers/PlaylistSynchronizer.cs
@@ -8,22 +8,39 @@ namespace PlaylistTransferTool.Synchronizers
     {
         private Playlist _playlist;
         private MusicLibraryContext _context;
+        private ConfiguratorValues _config;
 
-        public PlaylistSynchronizer(Playlist playlist, MusicLibraryContext context)
+        public PlaylistSynchronizer(Playlist playlist, MusicLibraryContext context, ConfiguratorValues config)
         {
             _playlist = playlist;
             _context = context;
+            _config = config;
         }
 
-        public SyncOperation Insert()
+        public SyncOperation Sync()
         {
             if(_context.Playlist.Where(p => p.PlaylistName == _playlist.PlaylistName).Any())
             {
-                LoggingUtils.GenerationLogWriteData($"Playlist {_playlist.PlaylistName} already exists.");
-                _playlist.PlaylistID = _context.Playlist.Where(
-                    p => p.PlaylistName == _playlist.PlaylistName
-                ).FirstOrDefault().PlaylistID;
-                return SyncOperation.Skip;
+                if (_config.mergePlaylistsWithSameName)
+                {
+                    LoggingUtils.GenerationLogWriteData($"Playlist '{_playlist.PlaylistName}' already exists.");
+                    _playlist.PlaylistID = _context.Playlist.Where(
+                        p => p.PlaylistName == _playlist.PlaylistName
+                    ).FirstOrDefault().PlaylistID;
+
+                    return SyncOperation.Skip;
+                }
+                else
+                {
+                    LoggingUtils.GenerationLogWriteData($"Overwriting playlist '{_playlist.PlaylistName}'");
+                    _context.PlaylistTracks.RemoveRange(_context.PlaylistTracks.Where(plt => plt.PlaylistID == _playlist.PlaylistID));
+                    _context.SaveChanges();
+
+                    _context.Playlist.Add(_playlist);
+                    _context.SaveChanges();
+
+                    return SyncOperation.Insert;
+                }
             }
             _context.Playlist.Add(_playlist);
             _context.SaveChanges();
