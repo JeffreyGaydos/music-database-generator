0494e1e4133c3766f4e34fbb4e31f2be6e89cdbf
JeffreyGaydos <jeff13gaydos@gmail.com>
2024-07-25 21:13:02 -0400

    Get this namespace out of here

--- PlaylistTransferTool/Configurator.cs
+++ PlaylistTransferTool/Configurator.cs
@@ -2,92 +2,88 @@
 using System.IO;
 using System;
 using System.Linq;
-using MusicDatabaseGenerator;
 
 namespace PlaylistTransferTool
 {
-    namespace MusicDatabaseGenerator
+    public class ConfiguratorValues
     {
-        public class ConfiguratorValues
-        {
-            public string playlistImportPath { get; private set; }
-            public string playlistExportPath { get; private set; }
-            public PlaylistType playlistExportType { get; private set; }
-            public bool mergePlaylistsWithSameName { get; private set; }
-            public bool deleteExistingPlaylists { get; private set; }
-            public bool exportOnly { get; private set; }
-            public bool findMissingTracks { get; private set; }
+        public string playlistImportPath { get; private set; }
+        public string playlistExportPath { get; private set; }
+        public PlaylistType playlistExportType { get; private set; }
+        public bool mergePlaylistsWithSameName { get; private set; }
+        public bool deleteExistingPlaylists { get; private set; }
+        public bool exportOnly { get; private set; }
+        public bool findMissingTracks { get; private set; }
 
-            public ConfiguratorValues(
-                string playlistImportPath,
-                string playlistExportPath,
-                PlaylistType playlistExportType,
-                bool mergePlaylistsWithSameName,
-                bool deleteExistingPlaylists,
-                bool exportOnly,
-                bool findMissingTracks
-            )
+        public ConfiguratorValues(
+            string playlistImportPath,
+            string playlistExportPath,
+            PlaylistType playlistExportType,
+            bool mergePlaylistsWithSameName,
+            bool deleteExistingPlaylists,
+            bool exportOnly,
+            bool findMissingTracks
+        )
+        {
+            this.playlistImportPath = playlistImportPath;
+            this.playlistExportPath = playlistExportPath;
+            this.playlistExportType = playlistExportType;
+            this.mergePlaylistsWithSameName = mergePlaylistsWithSameName;
+            this.deleteExistingPlaylists = deleteExistingPlaylists;
+            this.exportOnly = exportOnly;
+            this.findMissingTracks = findMissingTracks;
+        }
+    }
+
+    public class Configurator
+    {
+        private PlaylistType StringToPlaylistType(string name)
+        {
+            if (Enum.TryParse<PlaylistType>(name, out var type))
             {
-                this.playlistImportPath = playlistImportPath;
-                this.playlistExportPath = playlistExportPath;
-                this.playlistExportType = playlistExportType;
-                this.mergePlaylistsWithSameName = mergePlaylistsWithSameName;
-                this.deleteExistingPlaylists = deleteExistingPlaylists;
-                this.exportOnly = exportOnly;
-                this.findMissingTracks = findMissingTracks;
+                return type;
+            }
+            else
+            {
+                LoggingUtils.GenerationLogWriteData($"ERROR: Playlist type {name} is not supported. Supported playlist tpyes are:");
+                foreach (var e in Enum.GetNames(typeof(PlaylistType)))
+                {
+                    LoggingUtils.GenerationLogWriteData($" ^-- {e}");
+                }
+                return PlaylistType.None;
             }
         }
 
-        public class Configurator
+        public ConfiguratorValues HandleConfiguration()
         {
-            private PlaylistType StringToPlaylistType(string name)
-            {
-                if (Enum.TryParse<PlaylistType>(name, out var type))
-                {
-                    return type;
-                }
-                else
-                {
-                    LoggingUtils.GenerationLogWriteData($"ERROR: Playlist type {name} is not supported. Supported playlist tpyes are:");
-                    foreach (var e in Enum.GetNames(typeof(PlaylistType)))
-                    {
-                        LoggingUtils.GenerationLogWriteData($" ^-- {e}");
-                    }
-                    return PlaylistType.None;
-                }
-            }
+            IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile(Directory.GetParent("./") + "../../../appsettings.json").Build();
 
-            public ConfiguratorValues HandleConfiguration()
-            {
-                IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile(Directory.GetParent("./") + "../../../appsettings.json").Build();
+            var settings = config.GetSection("Settings").GetChildren().ToDictionary(r => r.Key, r => r.Value);
 
-                var settings = config.GetSection("Settings").GetChildren().ToDictionary(r => r.Key, r => r.Value);
+            ConfiguratorValues values = new ConfiguratorValues(
+                settings["playlistImportPath"] ?? "",
+                settings["playlistExportPath"] ?? "",
+                StringToPlaylistType(settings["playlistExportType"] ?? ""),
+                settings["mergePlaylistWithSameName"] == "True",
+                settings["deleteExistingPlaylists"] == "True",
+                settings["exportOnly"] == "True",
+                settings["findMissingTracks"] == "True"
+            );
 
-                ConfiguratorValues values = new ConfiguratorValues(
-                    settings["playlistImportPath"] ?? "",
-                    settings["playlistExportPath"] ?? "",
-                    StringToPlaylistType(settings["playlistExportType"] ?? ""),
-                    settings["mergePlaylistWithSameName"] == "True",
-                    settings["deleteExistingPlaylists"] == "True",
-                    settings["exportOnly"] == "True",
-                    settings["findMissingTracks"] == "True"
-                );
+            LoggingUtils.GenerationLogWriteData($"--== Ensure that your file structure on both of your devices is the same ==--");
 
-                LoggingUtils.GenerationLogWriteData($"--== Ensure that your file structure on both of your devices is the same ==--");
+            LoggingUtils.GenerationLogWriteData("_CONFIGURATION:__________________________________________________________________");
+            if(!values.exportOnly)
+                LoggingUtils.GenerationLogWriteData($"Will Import playlist files from: {values.playlistImportPath}");
+            LoggingUtils.GenerationLogWriteData($"Will Export playlist files to: {values.playlistExportPath}");
+            LoggingUtils.GenerationLogWriteData($"Will Export \"{values.playlistExportType}\" playlists");
+            LoggingUtils.GenerationLogWriteData($"Will {(values.mergePlaylistsWithSameName ? "" : "NOT ")}merge playlists with the same name NOTE: config not implemented");
+            LoggingUtils.GenerationLogWriteData($"Will {(values.deleteExistingPlaylists ? "delete" : "persist")} existing playlists from database");
+            if(!values.findMissingTracks)
+                LoggingUtils.GenerationLogWriteData($"Will attempt to find new paths for missing tracks. NOTE: NOT IMPLEMENTED");
+            LoggingUtils.GenerationLogWriteData("_________________________________________________________________________________");
 
-                LoggingUtils.GenerationLogWriteData("_CONFIGURATION:__________________________________________________________________");
-                if(!values.exportOnly)
-                    LoggingUtils.GenerationLogWriteData($"Will Import playlist files from: {values.playlistImportPath}");
-                LoggingUtils.GenerationLogWriteData($"Will Export playlist files to: {values.playlistExportPath}");
-                LoggingUtils.GenerationLogWriteData($"Will Export \"{values.playlistExportType}\" playlists");
-                LoggingUtils.GenerationLogWriteData($"Will {(values.mergePlaylistsWithSameName ? "" : "NOT ")}merge playlists with the same name NOTE: config not implemented");
-                LoggingUtils.GenerationLogWriteData($"Will {(values.deleteExistingPlaylists ? "delete" : "persist")} existing playlists from database");
-                if(!values.findMissingTracks)
-                    LoggingUtils.GenerationLogWriteData($"Will attempt to find new paths for missing tracks. NOTE: NOT IMPLEMENTED");
-                LoggingUtils.GenerationLogWriteData("_________________________________________________________________________________");
-
-                return values;
-            }
+            return values;
         }
     }
 }
