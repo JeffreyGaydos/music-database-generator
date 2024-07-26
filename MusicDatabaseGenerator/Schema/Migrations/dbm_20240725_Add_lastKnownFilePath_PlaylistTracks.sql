02ba2f100d6e2b6f0844e695dcd025a731379d78
JeffreyGaydos <jeff13gaydos@gmail.com>
2024-07-25 21:46:49 -0400

    Add migration script for new Last Known Path field

---
+++ MusicDatabaseGenerator/Schema/Migrations/dbm_20240725_Add_lastKnownFilePath_PlaylistTracks.sql
@@ -0,0 +1,13 @@
+﻿BEGIN TRAN
+
+USE MusicLibrary
+
+ALTER TABLE PlaylistTracks ADD LastKnownPath VARCHAR(260) NULL
+
+UPDATE PLTS
+SET LastKnownPath = MAIN.FilePath
+FROM MusicLibrary.dbo.PlaylistTracks PLTS
+LEFT JOIN MusicLibrary.dbo.Main MAIN ON MAIN.TrackID = PLTS.TrackID
+
+--ROLLBACK TRAN
+COMMIT TRAN
